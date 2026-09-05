8e0091e4e1773f1b1447425b9edaef9689b4d200
Dhadder7 <daniel.hadder@yahoo.com>
2026-09-05 12:34:04 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+window.GPT_LINKS = {
+
+  "DOI": "",
+
+  "German Coach": "",
+
+  "Chess Coach": "",
+
+  "Martial Arts Coach": "",
+
+  "Philosophy Mentor": "",
+
+  "Senior Army Leadership & Signal Mentor": "",
+
+  "Historian & Graduate History Coach": "",
+
+  "Constitution & American Government Tutor": "",
+
+  "Bible & Christianity Scholar": "",
+
+  "Learning, Reading & Knowledge Coach": "",
+
+  "Music Mentor": ""
+
+};
