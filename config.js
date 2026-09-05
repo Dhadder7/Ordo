7d43e5652d92a661fead554338c04db7dd8c3436
Dhadder7 <daniel.hadder@yahoo.com>
2026-09-05 12:42:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 window.GPT_LINKS = {
 
-  "DOI": "",
+  "DOI": "https://chatgpt.com/g/g-6a5d3b366b1c8191b929d850e2782373-doi",
 
-  "German Coach": "",
+  "German Coach": "https://chatgpt.com/g/g-6a9c1c7db1d881918704046fc2ceeca7-sprich-deutsch",
 
-  "Chess Coach": "",
+  "Chess Coach": "https://chatgpt.com/g/g-6a9c20048d0c819199b6b5b85d403ddb-chess",
 
-  "Martial Arts Coach": "",
+  "Martial Arts Coach": "https://chatgpt.com/g/g-6a9c20f605d88191b654ab8b9322fd7a-martial-arts-coach",
 
-  "Philosophy Mentor": "",
+  "Philosophy Mentor": "https://chatgpt.com/g/g-6a9c21fc9ed88191bd70ae50bbf5892f-philosophy-mentor",
 
-  "Senior Army Leadership & Signal Mentor": "",
+  "Senior Army Leadership & Signal Mentor": "https://chatgpt.com/g/g-6a9c239f41288191ae471c9dab1c3259-senior-army-leadership-signal-mentor",
 
-  "Historian & Graduate History Coach": "",
+  "Historian & Graduate History Coach": "https://chatgpt.com/g/g-6a9c2454c4e081918fd14158d1e50719-historian-graduate-history-coach",
 
-  "Constitution & American Government Tutor": "",
+  "Constitution & American Government Tutor": "https://chatgpt.com/g/g-6a9c256375488191af22001a9238f9d0-constitution-american-government",
 
-  "Bible & Christianity Scholar": "",
+  "Bible & Christianity Scholar": "https://chatgpt.com/g/g-6a9c25f27090819181b47417a15dbd3f-bible-christianity-scholar",
 
-  "Learning, Reading & Knowledge Coach": "",
+  "Learning, Reading & Knowledge Coach": "https://chatgpt.com/g/g-6a9c26cd4fc0819192375fd3cf764411-learning-reading-knowledge-coach",
 
-  "Music Mentor": ""
+  "Music Mentor": "https://chatgpt.com/g/g-6a9c27721000819183d0e5dd9cc6cb60-music-mentor"
 
 };
